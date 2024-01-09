d1d3de7d92cf9e9e35f4432a3cbac80680446aee
Luv Kumar <lovkumar120169@gmail.com>
2024-01-09 20:17:39 +0530

    card-section-completed

--- greenmentor-assignment/src/Components/SideMain.jsx
+++ greenmentor-assignment/src/Components/SideMain.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { TopNav } from './TopNav'
 import { TopSelect } from './TopSelect'
+import { Card } from './Card'
 
 export const SideMain = () => {
   return (
     <div style={{width:"100%"}}>
         <TopNav/>
         <TopSelect/>
+        <Card/>
     </div>
   )
 }
